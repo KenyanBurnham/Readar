fd9435590c9b8c84d357ae1eed81006df8e744ae
Kenyan Burnham <kenyan.burnham@ttu.edu>
2019-04-15 17:37:15 -0500

    Adding a new branch to remake the model

--- js/model/constructor.js
+++ js/model/constructor.js
@@ -11,7 +11,8 @@
         ------------------------------------------------------------------------
 =============================================================================**/
 
-function Word(word, syllable, size, breath) {
+function Word(wordIdentity, word, syllable, size, breath) {
+        this.wordIdentity = wordIdentity;
         this.word = word;
         this.syllable = syllable;
         this.size = length;
@@ -27,26 +28,23 @@ function Word(word, syllable, size, breath) {
         identity:       Unique key for the sentence
         source:         The original string
         words:          Array of Word objects
-        syllable:       - Remove -
         sumSyllables:   A sum of all syllables in the sentence
         sumWords:       A sum of all word lengths in sentence
-        sizes:          - Remove -
-        breath:         - sum of all breath units in sentence
-        breathPerWord:  - Remove -
-        mapOfWords:     - To Add - Each key of words in order
+        breath:         sum of all breath units in sentence
+        breathPerWord:  Array of breaths per word in sentence
+        mapOfWords:     Each key of words in order
         ------------------------------------------------------------------------
 =============================================================================**/
 
-function Sentence(identity, source, words, syllables, sumSyllables, sumWords, sizes, breath, breathPerWord){
-        this.identity = identity;
-        this.source = source;
+function Sentence(sentenceIdentity, sentenceSource, words, syllables, sumSyllables, sumWords, sizes, breath, breathPerWord){
+        this.sentenceIdentity = sentenceIdentity;
+        this.sentenceSource = source;
         this.words = words;
-        this.syllable = syllables;
         this.sumSyllables = sumSyllables;
         this.sumWords = sumWords;
-        this.sizes = sizes;
         this.breath = breath;
         this.breathPerWord = breathPerWord;
+        this.mapOfWords = mapOfWords;
 }
 
 /**=============================================================================
@@ -61,17 +59,48 @@ function Sentence(identity, source, words, syllables, sumSyllables, sumWords, si
         sumSyllables:   A sum of all syllables in the paragraph
         sumWords:       A sum of all word lengths in paragraph
         breath:         sum of all breath units in the paragraph
-        mapOfSentences: - To Add - Each key of sentences in order
+        mapOfSentences: Each key of sentences in order
         ------------------------------------------------------------------------
 =============================================================================**/
 
-function Paragraph(identity, source, sentences, sumSyllables, sumWords, breath, breathPerSentence, mapOfSentences){
-        this.identity = identity;
-        this.source = source;
+function Paragraph(paragraphIdentity, paragraphSource, sentences, sumSyllables, sumWords, breath, breathPerSentence, mapOfSentences){
+        this.paragraphIdentity = paragraphIdentity;
+        this.paragraphSource = paragraphSource;
         this.sentences = sentences;
         this.sumSyllables = sumSyllables;
         this.sumWords = sumWords;
         this.breath = breath;
         this.breathPerSentence = breathPerSentence;
         this.mapOfSentences = mapOfSentences;
+}
+
+/**=============================================================================
+        Body - Class Constructor
+
+        ------------------------------------------------------------------------
+            Properties:
+        ------------------------------------------------------------------------
+        bodySource:           Copy of original string input
+        paragraphs:           Array of all paragraphs in the Body
+        mapOfStringElements:  Each key of each Paragraph and Header in order
+        headers:              Array of all header strings in the Body
+        sumSyllables:         sum of all syllables in the Body
+        sumWords:             sum of all word length in the Body
+        breath:               Breath unit of the whole Body
+        breathWithoutHeader:  Breath unit of whole Body without headers, sets average
+        breathPerParagraph:   Breath unit of each Paragraph in order
+        ------------------------------------------------------------------------
+=============================================================================**/
+
+function Body(bodySource, paragraphs, mapOfStringElements, headers, sumSyllables, sumWords, breath, breathWithoutHeader, breathPerParagraph){
+        this.bodySource = bodySource;
+        this.paragraphs = paragraphs;
+        this.mapOfStringElements = mapOfStringElements;
+        this.headers = headers;
+        this.sumSyllables = sumSyllables;
+        this.sumWords = sumWords;
+        this.breath = breath;
+        this.breathWithoutHeader = breathWithoutHeader;
+        this.breathPerParagraph = breathPerParagraph;
+
 }
