1bd4c5084c4ad178110a5a396b9d3c6e757cf51c
KenyanBurnham <kenyan.burnham@ttu.edu>
2019-04-29 21:25:23 -0500

    Creating keypress event fubction
    For later live statistics

---
+++ js/rev3.js
@@ -0,0 +1,11 @@
+function handleNewKey(event){
+    // Whether character is shifted or not
+    shiftKey = event.shiftKey;
+    // Specific name via method
+    code = event.code;
+    // Specific character
+    key = event.key;
+    //Time stamp when entered
+    timeStamp = event.timeStamp;
+    console.log(event);
+}
