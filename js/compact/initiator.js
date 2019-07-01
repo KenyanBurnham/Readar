a8242f04c5e2f51ad3d5cce5b16abb635b6b7370
Kenyan Burnham <kenyan.burnham@ttu.edu>
2019-07-01 15:32:28 -0500

    initiator is not detected

--- js/compact/initiator.js
+++ js/compact/initiator.js
@@ -1,11 +1,11 @@
 
 //Initiator Function calls the first instance of Readar
 //Target specifies the DOM Object to target
-function InitiatorFunction(target){
+function initiatorFunction(target){
       //Grab source element and decouple text from DOM object.
       let decoupledSource = Decoupler.decouple(target);
       //Send to the processor object.
       Paragraphs.process(decoupledSource);
       //Return the output
-      Document.bodyDebug();
+      //Document.bodyDebug();
 }
